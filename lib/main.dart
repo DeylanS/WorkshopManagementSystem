ed90ee12abd04daf9716207cddb8a268831af4ac
DeylanS <82487648+DeylanS@users.noreply.github.com>
2025-06-17 02:22:24 +0800

    Update main.dart all modules

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -8,12 +9,18 @@ import 'package:flutter_stripe/flutter_stripe.dart';
 import 'Repositories/PaymentInfo.dart';
 import 'providers/inventory_provider.dart';
 import 'providers/schedule_provider.dart';
+import 'providers/rating_provider.dart';
+import 'providers/user_provider.dart';
 import 'pages/Payment/PaymentList.dart';
 import 'screens/login_page.dart';
 
 void main() async {
+  // This function is the one that will solve the problem.
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp();
+
+  // --- THE REST OF YOUR CODE REMAINS THE SAME ---
 
   Stripe.publishableKey = 'pk_test_51RZTlhELyXHhdRCoPA5COMaHxcQ338EyeIUJuzJHl295KrgoUJHs53c6ZXXzYiQnDsJobMSTswu9hD1sjfq2MtJO006AIYDNoo';
   await Stripe.instance.applySettings();
@@ -24,24 +31,23 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
-        // Provider for your PaymentInfo repository.
-        // Use Provider for objects that don't change/notify listeners.
+        // Your providers are configured correctly here.
         Provider<PaymentInfo>(
           create: (_) => PaymentInfo(),
         ),
-        
-        // ChangeNotifierProvider for Inventory management.
         ChangeNotifierProvider<InventoryProvider>(
           create: (_) => InventoryProvider(),
         ),
-
-        // ChangeNotifierProvider for Schedule management.
         ChangeNotifierProvider<ScheduleProvider>(
           create: (_) => ScheduleProvider(),
         ),
+        ChangeNotifierProvider<UserProvider>(
+          create: (_) => UserProvider(),
+        ),
+        ChangeNotifierProvider<RatingProvider>(
+          create: (_) => RatingProvider(),
+        ),
       ],
-      // The child of MultiProvider is your main App widget.
-      // All widgets within MyApp can now access all three providers.
       child: const MyApp(),
     ),
   );
